eb645438db28c7fb0200b45b1e7ae7cc488b0524
catwhitmer <catwhitmer@gmail.com>
2020-12-08 08:46:28 -0500

    added union type function

--- ts.ts
+++ ts.ts
@@ -14,3 +14,20 @@ function Log(target: any, propertyName: string | Symbol) {
     console.log('Property decorator');
     console.log(target, propertyName);
 }
+
+//Union Type TS
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    } 
+    return result;
+};
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Tim', 'Cat');
+console.log(combinedNames);
