06c33d514e323411bbcb768c2176fd8907c54396
catwhitmer <catwhitmer@gmail.com>
2020-12-14 13:08:45 -0500

    added custom type

--- ts.ts
+++ ts.ts
@@ -45,3 +45,31 @@ class Department {
 const accounting = new Department('Accounting');
 
 console.log(accounting);
+
+
+//custom type
+type Combinable = number | string;
+type Conversion = 'as-number' | 'as-text';
+
+function combined(
+    input1: Combinable, 
+    input2: Combinable,
+    resultConversion: Conversion
+    ) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    } 
+    return result;
+};
+
+const combinedAge = combined(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combined('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedName = combined('Tim', 'Cat', 'as-text');
+console.log(combinedNames);
