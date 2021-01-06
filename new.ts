0753d31ed0530f29a467880992851f7523c076c2
catwhitmer <catwhitmer@gmail.com>
2021-01-06 13:14:00 -0500

    added a void return type

--- new.ts
+++ new.ts
@@ -10,3 +10,16 @@ function greet(user: User) {
 function isOlder(user: User, checkAge: number) {
   return checkAge > user.age;
 }
+
+//Return Types / Void
+
+function addNumber(n1: number, n2: number) {
+   return n1 + n2; 
+}
+
+//: void because no return statement
+function printResults(num: number) {
+    console.log('Result:' + num);
+}
+
+printResults(addNumber(5, 12));
