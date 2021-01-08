7876c2470a2adabeb41c0a35973048385e898aa0
catwhitmer <catwhitmer@gmail.com>
2021-01-08 13:02:35 -0500

    added an autobind decorator

--- new.ts
+++ new.ts
@@ -45,3 +45,39 @@ const accounting = new Department('Accounting');
 console.log(accounting);
 
 accounting.describe();
+
+
+
+
+//TS Autobind Decorator
+
+function Autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFunc = originalMethod.bind(this);
+      return boundFunc;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
